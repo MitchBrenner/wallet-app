2688cc4b535b73a825bb054f646cf5745c7d7cfe
Mitchell Brenner <mitchbrenner8@gmail.com>
2025-08-03 17:53:34 -0600

    Initialized API

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Wallet App 💵
+
+- node backend with express
+- using nodemon so you don't have to kill server, create script under package.json
+
+  - nodemon watches for changes
+
+- use dotenv package to access file check [server.js](./backend/server.js)
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 // const express = require("express");
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+const PORT = process.env.PORT || 5001;
 
 app.get("/", (req, res) => {
   res.send("It's working!");
 });
 
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });

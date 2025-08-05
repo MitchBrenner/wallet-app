22e15cf34276ac5b3b2e9f0e13aa385e60e6ef8e
Mitchell Brenner <mitchbrenner8@gmail.com>
2025-08-05 16:28:09 -0600

    Updated frontend

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,3 +1,5 @@
+import { Image } from "expo-image";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -10,6 +12,11 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href={"/about"}>About</Link>
+      <Image
+        source={require("@/assets/images/react-logo.png")}
+        style={{ width: 100, height: 100 }}
+      />
     </View>
   );
 }
